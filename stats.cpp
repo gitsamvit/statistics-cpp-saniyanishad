c2af814128bdcc2a8c792578267fb8c504098090
saniyanishad <105099328+saniyanishad@users.noreply.github.com>
2022-05-08 13:03:08 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,12 +1,34 @@
 #include "stats.h"
 #include<cmath.h>
-
+#include<iostream>
 Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
     //Implement statistics here
-    auto computedstats;
-    int x=0;
-    float epsilon;
-    int arry={};
-    
-  
+    //auto computedstats;
+    //int x=0;
+    //float epsilon;
+    //int arry{}
+ //}
+int main(){
+    int i=1;
+    int n;
+    while(cin>>n){
+        int min;
+        int max;
+       cin>>max;
+        min=max;
+        for (int j=0;j<n-1;j++){
+            int temp;
+            cin>>temp;
+            if(temp<min){
+                min = temp;
+            }
+            if(temp>max);
+            max = temp;
+        }
+    }
+    cout<<"case"<<i<<":"<<min<<""<<max<<""max-min<<endl;
+    i++;
 }
+}
+    
+            
